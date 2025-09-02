3247097eb10018951b4c1e8bfe73069ef3f32e6c
Toshiba Mastru <ggostem42@gmail.com>
2025-09-02 14:58:27 +0300

    Update client

--- msi-ec-client/src/lib.rs
+++ msi-ec-client/src/lib.rs
@@ -1,23 +1,47 @@
 use msi_ec_types::{FanMode, OnOff, ShiftMode};
-use zbus::proxy;
+use zbus::{Result, proxy};
 
 #[proxy(
     interface = "by.toshibam.MsiEc",
     default_service = "by.toshibam.MsiEc",
     default_path = "/by/toshibam/MsiEc"
 )]
-pub trait MsiEc {
-    fn get_fan_mode(&self) -> zbus::Result<FanMode>;
-    fn set_fan_mode(&self, mode: FanMode) -> zbus::Result<()>;
-    fn get_available_fan_modes(&self) -> zbus::Result<Vec<FanMode>>;
+trait MsiEc {
+    //. fan_mode
 
-    fn get_shift_mode(&self) -> zbus::Result<ShiftMode>;
-    fn set_shift_mode(&self, mode: ShiftMode) -> zbus::Result<()>;
-    fn get_available_shift_modes(&self) -> zbus::Result<Vec<ShiftMode>>;
+    #[zbus(property)]
+    fn fan_mode(&self) -> Result<FanMode>;
 
-    fn get_cooler_boost(&self) -> zbus::Result<OnOff>;
-    fn set_cooler_boost(&self, state: OnOff) -> zbus::Result<()>;
+    #[zbus(property)]
+    fn set_fan_mode(&self, mode: FanMode) -> Result<()>;
 
-    fn get_super_battery(&self) -> zbus::Result<OnOff>;
-    fn set_super_battery(&self, state: OnOff) -> zbus::Result<()>;
+    #[zbus(property)]
+    fn available_fan_modes(&self) -> Result<Vec<FanMode>>;
+
+    //. shift_mode
+
+    #[zbus(property)]
+    fn shift_mode(&self) -> Result<ShiftMode>;
+
+    #[zbus(property)]
+    fn set_shift_mode(&self, mode: ShiftMode) -> Result<()>;
+
+    #[zbus(property)]
+    fn available_shift_modes(&self) -> Result<Vec<ShiftMode>>;
+
+    //. cooler_boost
+
+    #[zbus(property)]
+    fn cooler_boost(&self) -> Result<OnOff>;
+
+    #[zbus(property)]
+    fn set_cooler_boost(&self, state: OnOff) -> Result<()>;
+
+    //. super_battery
+
+    #[zbus(property)]
+    fn super_battery(&self) -> Result<OnOff>;
+
+    #[zbus(property)]
+    fn set_super_battery(&self, state: OnOff) -> Result<()>;
 }
